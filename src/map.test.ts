fb47b04f8c0783e05b54c5a594808b6c75a5f4f4
Radenko Vucanovic <radenkovuc@gmail.com>
2024-01-27 13:26:13 +0100

    Update map.test.ts

--- src/map.test.ts
+++ src/map.test.ts
@@ -2,5 +2,5 @@ import { collectLettersAndPath } from './map';
 
 test('Test', () => {
   //Initial test
-  expect(collectLettersAndPath([[], []])).toStrictEqual({ collectedLetters: '', path: '' });
+  expect(collectLettersAndPath([[], []])).toStrictEqual({ collectedLetters: '1', path: '' });
 });
